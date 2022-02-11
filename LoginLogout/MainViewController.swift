2b2f15f084c08d96337ac23cccc5529f3cd54a2e
Vladimir Usov <vlumailbox@gmail.com>
2022-02-11 23:54:55 +0300

    Code optimization

--- LoginLogout/MainViewController.swift
+++ LoginLogout/MainViewController.swift
@@ -14,32 +14,41 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var welcomeTF: UILabel!
     
-    
+    private let userName = "Admin"
+    private let passWord = "mypass"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        passwordTF.isSecureTextEntry = true
     }
-
 
     @IBAction func touchLoginButton() {
         guard let user = userNameTF.text else {return}
-        welcomeTF.text = "Welcome, " + user
-    }
-    
-    
-    @IBAction func showAlertUserName(_ sender: UIButton) {
-        let alertController = UIAlertController(
-            title: "input user name:",
-            message: "admin",
-            preferredStyle: .alert
-        )
-        let alertOKAction = UIAlertAction(title: "OK", style: .default)
-        alertController.addAction(alertOKAction)
+        guard let pass = passwordTF.text else {return}
         
-        present(alertController, animated: true)
+        if user == userName && pass == passWord {
+            welcomeTF.text = "Welcome, " + user + "!"
+        } else {
+            showAlert(title: "Access denied", message: "Wrong user name or password")
+        }
     }
     
+    @IBAction func getUserName() {
+        showAlert(title: "Enter user name:", message: userName)
+    }
+    
+    @IBAction func getPassword() {
+       showAlert(title: "Enter password:", message: passWord)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let buttonOK = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(buttonOK)
+        
+        present(alert, animated: true)
+    }
 }
 
